a739c470772e744c6e8f814527139ebd028a2ac3
jbyks <jochen.boeykens@gmail.com>
2017-03-24 16:30:48 +0100

    assignment-19-bloccit: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,15 @@ posts = Post.all
   )
 end
 
+50.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: RandomData.random_number
+  )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
